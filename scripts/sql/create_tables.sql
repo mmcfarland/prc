e207da18bbe91fe7bdc84aa26c837729ef669381
Matthew McFarland <mmcfarland@gmail.com>
2013-05-06 22:10:33 -0400

    Table scripts

---
+++ scripts/sql/create_tables.sql
@@ -0,0 +1,17 @@
+create table if not exists collections (
+	id 		serial PRIMARY KEY,
+	tile 		varchar(255) NOT NULL,
+	description	text,
+	owner 		varchar(55),
+	public 		boolean,
+	created 	timestamp DEFAULT current_timestamp,
+	modified 	timestamp DEFAULT current_timestamp
+);
+
+create index collectionid_idx on collections (id);
+
+create table if not exists collection_parcels (
+	collectionid	integer NOT NULL,
+	parcelid	integer NOT NULL
+);
+create index collection_parcels_idx on collection_parcels (collectionid);
