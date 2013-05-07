e94098cb8bbb2d886c69e3a6d9fe5a34b0809a05
Matthew McFarland <mmcfarland@gmail.com>
2013-05-06 22:09:57 -0400

    Factor out common error response

--- controllers.go
+++ controllers.go
@@ -9,18 +9,22 @@ import (
 	"strconv"
 )
 
+func ResponseWithError(id int, err error, w http.ResponseWriter, name string) {
+	if err == sql.ErrNoRows {
+		http.Error(w, "Parcel Not Found", 404)
+		log.Printf("Could not find parcel: %d", id)
+	} else {
+		log.Println(err)
+		http.Error(w, "", 500)
+	}
+}
+
 func ParcelDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 	log.Printf("Get parcel: %d", id)
 	if p, err := ParcelById(id); err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, "Parcel Not Found", 404)
-			log.Printf("Could not find parcel: %d", id)
-		} else {
-			log.Println(err)
-			http.Error(w, "", 500)
-		}
+		ResponseWithError(id, err, w, "Parcel")
 	} else {
 		b, _ := json.Marshal(p)
 		w.Write(b)
