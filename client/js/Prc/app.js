c284e03d7ccfd1c75178fab33043068fb27c640b
Matthew McFarland <mmcfarland@gmail.com>
2013-05-18 23:34:40 -0400

    Show a map on startup

--- client/js/Prc/app.js
+++ client/js/Prc/app.js
@@ -8,6 +8,10 @@
 
         init: function () {
             new N.TemplateLoader().load(N.app.tmpl);
+            N.app.models.map = new N.models.Map();
+            N.app.views.map = new N.views.Map({
+                model: N.app.models.map
+            });
         }
     };
 }(Prc));
