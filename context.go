6a988d90d1556da2317fb6230ce829c320e489f2
Matthew McFarland <mmcfarland@gmail.com>
2013-05-23 23:21:42 -0400

    Add convenience methods on context for session values

--- context.go
+++ context.go
@@ -20,3 +20,27 @@ func NewContext(r *http.Request) (*Context, error) {
 		Session: s,
 	}, err
 }
+
+func (c *Context) Login(u *User, r *http.Request, w http.ResponseWriter) {
+	c.Session.Values["loggedin"] = true
+	c.Session.Values["user"] = u.Username
+	c.Session.Save(r, w)
+}
+
+func (c *Context) Logout(r *http.Request, w http.ResponseWriter) {
+	c.Session.Values["loggedin"] = false
+	c.Session.Values["user"] = nil
+	c.Session.Save(r, w)
+}
+
+func (c *Context) IsLoggedIn() bool {
+	return c.Session.Values["loggedin"] == true
+}
+
+func (c *Context) GetUsername() string {
+	if str, ok := c.Session.Values["use"].(string); ok {
+		return str
+	} else {
+		return ""
+	}
+}
