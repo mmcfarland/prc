25b44cad0224f45102c39e525daa45bee206a7a4
Matthew McFarland <mmcfarland@gmail.com>
2013-05-23 23:24:02 -0400

    Launc the login/user views on startup

--- client/js/Prc/app.js
+++ client/js/Prc/app.js
@@ -19,6 +19,29 @@
                 model: N.app.models.map,
                 search: N.app.models.search
             });
+
+            this.setupUser();
+        }, 
+        
+        setupUser: function() {
+            N.app.models.user = new N.models.User(N.bootstrap.user);
+            N.app.views.login = new N.views.Login({
+                model: N.app.models.user,
+                el: $('#login-container')[0],
+                tmpl: {
+                    login: N.app.tmpl["template-login"]
+                }
+            });
+
+            N.app.views.loginForm = new N.views.LoginForm({
+                model: N.app.models.user,
+                tmpl: {
+                    loginForm: N.app.tmpl["template-login-form"],
+                    logoutForm: N.app.tmpl["template-logout-form"]
+                },
+                el: $('#login-dropdown')[0]
+            });
+
         }
     };
 }(Prc));
