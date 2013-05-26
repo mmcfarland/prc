84b39cadb119a6784ac94a62daafd5913f127390
Matthew McFarland <mmcfarland@gmail.com>
2013-05-26 18:05:52 -0400

    Add opa table structure - straight from arcgis at the city

--- scripts/sql/create_tables.sql
+++ scripts/sql/create_tables.sql
@@ -23,3 +23,34 @@ create table if not exists users (
 	joined		timestamp DEFAULT current_timestamp
 );
 create index on users (username);
+
+create table if not exists opa (
+	acct_num	varchar(255),
+	address		varchar(255),
+	unit		varchar(255),
+	homestd_ex	varchar(255),
+	prop_cat	varchar(255),
+	prop_type	varchar(255),
+	num_stor	real,
+	mktval_13	integer,
+	landval_13	integer,
+	impval_13	integer,
+	abat_ex_13	integer,
+	mktval_14	integer,
+	landval_14	integer,
+	impval_14	integer,
+	abat_ex_14	integer,
+	plotarea	real,
+	zone		varchar(255),
+	extcond		varchar(255),
+	totdwellarea	integer,
+	ownr_nam	varchar(255),
+	secd_nam	varchar(255),
+	dist_from	varchar(255),
+	ploc_zip5	varchar(255),
+	titl_date	date,
+	sale_price	integer,
+	geom		geometry(Point, 4326)
+);
+
+create index on opa (acct_num);
